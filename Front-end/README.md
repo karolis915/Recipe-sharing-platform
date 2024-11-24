96aef1aec091e5b0b5568d724e14143fbad386ba
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-11-24 21:03:22 +0200

    first commit

---
+++ Front-end/README.md
@@ -0,0 +1,11 @@
+Sveiki!
+
+Čia bus receptų dalinimosi platforma.
+Projektas kuriamas naudojant React JS (front-end) ir Java (back-end) technologijas.
+
+Autoriai:
+Emilija
+Rolandas
+Nerimantas
+(Visų projeto dalyvių prašom įrašyti savo vardą :)
+
---
+++ Front-end/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1>Sveiki atvykę į receptų dalinimosi puslapį!</h1>
+      
+        <p>
+          Čia bus receptų dalinimosi vieta!
+        </p>
+      
+      
+    </>
+  )
+}
+
+export default App
