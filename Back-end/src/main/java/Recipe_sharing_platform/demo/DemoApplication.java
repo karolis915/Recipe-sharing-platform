28a0b2f7956397bbcb891f705957af84b14a76c8
Nerimantas7 <nerimantas.tarabilda@stud.techin.lt>
2024-11-26 12:03:10 +0200

    Added back-end

---
+++ Back-end/src/main/java/Recipe_sharing_platform/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Recipe_sharing_platform.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
